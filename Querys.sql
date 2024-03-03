50adb50becb025050aa07711977c653e951c2cea
Luis A Lopez Atencio <luisangellopezatencio@outlook.com>
2024-03-03 17:48:03 -0500

    Modificado Query.sql había subido el archivo errado al repositorio

--- Querys.sql
+++ Querys.sql
@@ -1,30 +1,57 @@
-CREATE TABLE inventario (
-    id_producto SERIAL PRIMARY KEY,
-    nombre VARCHAR(255),
-    categoria VARCHAR(255),
-    precio DECIMAL,
-    cantidad_stock INTEGER,
-    fecha_ultima_reposicion DATE
-);  
+CREATE TABLE cliente (
+	ID serial primary key,
+	Nombre text not null,
+	Telefono integer
+);
 
-INSERT INTO inventario (id_producto, nombre, categoria, precio, cantidad_stock, fecha_ultima_reposicion) VALUES
-(1, 'Leche Entera', 'Lácteos', 1.20, 25, '2023-11-10'),
-(2, 'Pan Integral', 'Panadería', 1.50, 15, '2023-11-15'),
-(3, 'Manzanas Rojas', 'Frutas', 0.30, 50, '2023-11-16'),
-(4, 'Cereal de Avena', 'Desayuno', 2.50, 10, '2023-11-12'),
-(5, 'Yogur Natural', 'Lácteos', 0.80, 30, '2023-11-17'),
-(6, 'Tomates', 'Verduras', 0.25, 40, '2023-11-18'),
-(7, 'Pollo Entero', 'Carnes', 5.00, 20, '2023-11-14'),
-(8, 'Atún en Lata', 'Conservas', 1.75, 50, '2023-11-11'),
-(9, 'Arroz Blanco', 'Almacén', 1.00, 60, '2023-11-13'),
-(10, 'Huevos (docena)', 'Lácteos', 1.50, 18, '2023-11-19'),
-(11, 'Chocolate en Barra', 'Snacks', 1.20, 35, '2023-11-15'),
-(12, 'Espinacas', 'Verduras', 0.60, 5, '2023-11-16'),
-(13, 'Pasta Penne', 'Almacén', 0.90, 25, '2023-11-17'),
-(14, 'Aceite de Oliva', 'Almacén', 3.50, 20, '2023-11-18'),
-(15, 'Agua Mineral', 'Bebidas', 0.50, 100, '2023-11-19');
+CREATE TABLE sucursal (
+	ID serial primary key,
+	Nombre text not null,
+	Direccion text
+);
+
+CREATE TABLE orden (
+    ID serial PRIMARY KEY,
+    Cliente_ID INTEGER,
+    Sucursal_ID INTEGER,
+    Fecha_orden DATE NOT NULL,
+    Total REAL NOT NULL,
+    FOREIGN KEY (Cliente_ID) REFERENCES cliente(ID),
+    FOREIGN KEY (Sucursal_ID) REFERENCES sucursal(ID)
+);
+
+CREATE TABLE categoria (
+	ID serial PRIMARY KEY,
+	Nombre TEXT NOT NULL
+);
+
+CREATE TABLE producto (
+	ID serial PRIMARY KEY,
+	Nommbre TEXT NOT NULL,
+	Marca TEXT,
+	Categoria_ID INTEGER,
+	Precio_unitario REAL,
+	FOREIGN KEY (Categoria_ID) REFERENCES categoria(ID)
+);
+
+CREATE TABLE stock (
+	ID serial PRIMARY KEY,
+	Sucursal_ID INTEGER,
+	Producto_ID INTEGER,
+	Total REAL NOT NULL,
+	FOREIGN KEY (Sucursal_ID) REFERENCES sucursal(ID),
+	FOREIGN KEY (Producto_ID) REFERENCES producto(ID)
+);
+
+CREATE TABLE item (
+	ID serial PRIMARY KEY,
+	Orden_ID INTEGER,
+	Producto_ID INTEGER,
+	Cantidad REAL NOT NULL,
+	Monto_venta REAL NOT NULL,
+	FOREIGN KEY (Orden_ID) REFERENCES orden(ID),
+	FOREIGN KEY (Producto_ID) REFERENCES producto(ID)
+);
 
 
-select * from inventario
-where cantidad_stock < 10
 
